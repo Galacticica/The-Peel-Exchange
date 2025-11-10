0fdf158dde0b5bfe51972bdd3b99700643196034
Reagan Zierke <71458762+Galacticica@users.noreply.github.com>
2025-11-10 12:28:34 -0600

    Update run-cron.sh

--- run-cron.sh
+++ run-cron.sh
@@ -5,8 +5,6 @@ cd /app
 
 echo "Cron worker starting..."
 
-# Export all current environment variables to a file that cron can source
-# This ensures DB credentials and other secrets are available to cron jobs
 printenv | grep -v "no_proxy" > /etc/environment
 
 echo "Registering crontab entries..."
