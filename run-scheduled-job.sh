7be0c353eaddf625c08e9366a48b3acf60880037
Reagan Zierke <71458762+Galacticica@users.noreply.github.com>
2025-11-10 12:28:07 -0600

    Clean up comments in run-scheduled-job.sh
    Removed comment about running Django commands.

--- run-scheduled-job.sh
+++ run-scheduled-job.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-# Script to run Django management commands on Fly.io scheduled machines
 set -euo pipefail
 
 cd /app
 
-# Run the command passed as argument
 python manage.py "$@"
